6d850333c8f7324a64dbcd97bf52117824a6979c
Andrew Gilliland <andrewpgilliland@gmail.com>
2025-10-01 19:39:28 -0500

    feat: adds button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
       <h1>Events</h1>
+      <section>
+        <Button
+          variant="default"
+          onClick={() => {
+            console.log("Create Event clicked");
+          }}
+        >
+          Create Event
+        </Button>
+      </section>
     </>
   );
 }
